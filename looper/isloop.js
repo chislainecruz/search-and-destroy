b367a7570b0f2e2334e134cddb0723f1d868d263
Chislaine Cruz <chislaine.cruz@gmail.com>
2020-05-28 18:16:43 -0400

    so far

--- looper/isloop.js
+++ looper/isloop.js
@@ -1,10 +1,18 @@
-'use strict';
+"use strict";
 
 //Complete this algo
 const isLoop = (linkedlist) => {
-
+  let headNode = linkedlist.head;
+  let nextNode = headNode.next;
+  while (nextNode !== headNode) {
+    if (nextNode === null || headNode === null) {
+      return false;
+    }
+    headNode = headNode.next;
+    nextNode = nextNode.next.next; //nextNode = headNode.next; should work for this line but it does not. (?)
+  }
+  return true;
 };
-
 
 /*
 EXTRA CREDIT:
@@ -14,4 +22,4 @@ This function should return the Node value the loop begins at
 Remember to write some test specs too!
 
 */
-module.exports = isLoop
+module.exports = isLoop;
